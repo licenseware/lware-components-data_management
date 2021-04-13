5208d5ad21c60ab63f76182085c9ec5ff8c2a091
ClimenteA <climente.alin@gmail.com>
2021-04-13 08:37:22 +0300

    new funcs for dm

--- src/data_management.py
+++ src/data_management.py
@@ -1,10 +1,227 @@
 import os
-import logging
+import uuid
 import json
+import warnings
+import logging, traceback
 from bson.json_util import dumps
-from pymongo import errors
 from marshmallow import ValidationError
-from pymongo import MongoClient
+from pymongo import (
+    MongoClient,
+    ReturnDocument,
+    errors
+)
+
+
+
+def deprecated(message):
+  def deprecated_decorator(func):
+      def deprecated_func(*args, **kwargs):
+          warnings.warn("{} is a deprecated function. {}".format(func.__name__, message),
+                        category=DeprecationWarning,
+                        stacklevel=2)
+          warnings.simplefilter('default', DeprecationWarning)
+          return func(*args, **kwargs)
+      return deprecated_func
+  return deprecated_decorator
+
+
+
+MONGO_ROOT_USERNAME = 'licensewaredev'
+MONGO_ROOT_PASSWORD ='license123ware'
+MONGO_DATABASE_NAME='db'
+MONGO_HOSTNAME= 'localhost' #for a docker environment use 'mongodb' (service name)
+MONGO_PORT=27017
+
+os.environ['MONGO_DATABASE_NAME'] = MONGO_DATABASE_NAME
+os.environ['MONGO_CONNECTION_STRING'] = f"mongodb://{MONGO_ROOT_USERNAME}:{MONGO_ROOT_PASSWORD}@{MONGO_HOSTNAME}:{MONGO_PORT}"
+
+
+class MongoData:
+
+    connection = MongoClient(os.getenv("MONGO_CONNECTION_STRING"))
+
+    @classmethod
+    def get_collection(cls, collection, db):
+        c = cls()
+        if db: return c.connection[db][collection]
+        return c.connection[os.getenv("MONGO_DATABASE_NAME")][collection]
+
+
+    @staticmethod
+    def validate_data(schema, data):  
+        # try:
+
+        if isinstance(data, dict):
+            data = schema().load(data)
+
+        if isinstance(data, list):
+            data = schema(many=True).load(data)
+
+        return MongoData.append_uuid(data)
+    
+        # except ValidationError as e: 
+        #     print(traceback.format_exc())
+        #     print(str(e))
+
+
+    @staticmethod
+    def append_uuid(data):
+        # Appends uuid4 ids for each dict in list
+        # [{"field": "data", "field2": {"f": "d"}}, etc] 
+        # => [{"_id": uuid4, "field": "data", "field2": {"f": "d"}}, etc] 
+
+        if isinstance(data, list):
+            data = [MongoData.uuid_to_dict(d) for d in data]
+            return data
+        return MongoData.uuid_to_dict(data)
+
+    @staticmethod
+    def uuid_to_dict(data):
+        if isinstance(data, dict):
+            if "_id" not in data.keys():
+                data.update({"_id": str(uuid.uuid4())})
+                return data
+        return data
+
+
+    @classmethod
+    def insert(cls, *, schema, collection, data, db_name=None):
+        """
+            Insert validated documents in database.
+
+            :schema     - Marshmallow schema class 
+            :collection - collection name
+            :data       - data in dict or list of dicts format
+            :db_name    - specify other db if needed, by default is MONGO_DATABASE_NAME from .env
+            
+            returns a list of ids inserted in the database in the order they were added
+        """
+        c = cls()
+           
+        collection = c.get_collection(collection, db_name)
+        data = MongoData.validate_data(schema, data)
+
+        if isinstance(data, dict):
+            return [collection.insert_one(data).inserted_id]
+    
+        if isinstance(data, list):
+            return collection.insert_many(data).inserted_ids
+
+        return data
+    
+
+    @classmethod
+    def fetch(cls, *, collection, match, as_list=False, db_name=None):
+        """
+            Get data from mongo, based on match dict or string id.
+            
+            :collection - collection name
+            :match      - id as string or dict filter
+            :as_list    - return data found as list by default returns a generator
+            :db_name    - specify other db if needed by default is MONGO_DATABASE_NAME from .env
+
+            returns a generator of documents found or if as_list=True a list of documents found  
+
+        """
+        c = cls()
+        
+        by_id = False
+        if isinstance(match, str): 
+            match = {"_id": match}
+            by_id = True
+            
+        collection = c.get_collection(collection, db_name)
+        found_docs = collection.find(match)
+        
+        if as_list or by_id:
+            data = [r for r in found_docs]
+            if by_id: data = data[0] if len(data) == 1 else data
+            return data
+        
+        return (r for r in found_docs) # generator
+        
+
+    @classmethod
+    def update(cls, *, collection, match, new_data, db_name=None):
+        """
+            
+            :collection  - collection name
+            :match       - id as string or dict filter
+            :new_data    - data dict which needs to be updated
+            :db_name     - specify other db if needed by default is MONGO_DATABASE_NAME from .env
+            
+            returns number of modified documents
+
+        """
+
+        c = cls()
+
+        if isinstance(match, str): 
+            match = {"_id": match}
+        
+        collection = c.get_collection(collection, db_name)
+        
+        updated_docs_nbr = collection.update_many(
+            filter=match,
+            update={"$set": new_data},
+            upsert=True
+        ).modified_count
+        
+        return updated_docs_nbr
+
+
+    
+    @classmethod
+    def delete(cls, *, collection, match, new_data, db_name=None):
+        """
+            
+            :collection  - collection name
+            :match       - id as string or dict filter
+            :new_data    - data dict which needs to be updated
+            :db_name     - specify other db if needed by default is MONGO_DATABASE_NAME from .env
+            
+            returns number of modified documents
+
+        """
+
+        c = cls()
+
+        if isinstance(match, str): 
+            match = {"_id": match}
+        
+        collection = c.get_collection(collection, db_name)
+        
+        updated_docs_nbr = collection.update_many(
+            filter=match,
+            update={"$mod": new_data},
+            upsert=True
+        ).modified_count
+        
+        return updated_docs_nbr
+
+
+
+    # def delete(self, id=None, data=None, filter=None):
+    #     pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 class DataManagement:
@@ -59,6 +276,50 @@ class DataManagement:
             self.db(self.default_db)
 
     
+    def insert_one(self, json_data):
+        if not json_data:
+            return {"status": "fail", "message": "No input data provided"}, 400
+        try:
+            data = self.document_schema.load(json_data)
+            collection = self._collection_db(self.collection_name)
+            collection.insert_one(data)
+            return {"status": "success", "message": "Created new document.", "document": data}, 202
+        except ValidationError as err:
+            print(err)
+            return err.messages, 422
+        finally:
+            self.switch_to_default_db()
+
+
+    def insert_data(self, json_data):
+        if not json_data:
+            return {"status": "fail", "message": "No input data provided"}, 400
+        try:
+            data = self.document_schema.load(json_data)
+            collection = self.database[self.collection]
+            collection.insert_one(data)
+            return {"status": "success", "message": "Created new document.", "document": data}, 202
+        except ValidationError as err:
+            logging.warning(err)
+            return err.messages, 422
+        finally:
+            self.switch_to_default_db()
+
+
+    def insert_many(self, json_data):
+        if not json_data:
+            return {"status": "fail", "message": "No input data provided"}, 400
+        try:
+            collection = self._collection_db(self.collection_name)
+            collection.insert_many(json_data)
+            return {"status": "success", "message": "Created new documents."}, 202
+        except ValidationError as err:
+            print(err)
+            return err.messages, 422
+        finally:
+            self.switch_to_default_db()
+
+
     def get_by_id(self, _id):
         try:
             collection = self._collection_db(self.collection_name)
@@ -100,50 +361,6 @@ class DataManagement:
             return results_data, 200
         except errors.ConnectionFailure as e:
             return {"status": "fail", "message": "errors in connection"}, 500
-        finally:
-            self.switch_to_default_db()
-
-
-    def insert_one(self, json_data):
-        if not json_data:
-            return {"status": "fail", "message": "No input data provided"}, 400
-        try:
-            data = self.document_schema.load(json_data)
-            collection = self._collection_db(self.collection_name)
-            collection.insert_one(data)
-            return {"status": "success", "message": "Created new document.", "document": data}, 202
-        except ValidationError as err:
-            print(err)
-            return err.messages, 422
-        finally:
-            self.switch_to_default_db()
-
-
-    def insert_many(self, json_data):
-        if not json_data:
-            return {"status": "fail", "message": "No input data provided"}, 400
-        try:
-            collection = self._collection_db(self.collection_name)
-            collection.insert_many(json_data)
-            return {"status": "success", "message": "Created new documents."}, 202
-        except ValidationError as err:
-            print(err)
-            return err.messages, 422
-        finally:
-            self.switch_to_default_db()
-
-
-    def insert_data(self, json_data):
-        if not json_data:
-            return {"status": "fail", "message": "No input data provided"}, 400
-        try:
-            data = self.document_schema.load(json_data)
-            collection = self.database[self.collection]
-            collection.insert_one(data)
-            return {"status": "success", "message": "Created new document.", "document": data}, 202
-        except ValidationError as err:
-            logging.warning(err)
-            return err.messages, 422
         finally:
             self.switch_to_default_db()
 
@@ -242,3 +459,6 @@ except:
     # Environment variables not specified. 
     # Needed data must be provided on class instantiation
     dm = None 
+
+
+
--- tests/test_dm.py
+++ tests/test_dm.py
@@ -1,9 +1,11 @@
 import pytest
 from assertpy import assert_that
 import os
-from src.data_management import DataManagement, dm
+from src.data_management import DataManagement, dm, MongoData
 from marshmallow import Schema, fields
 import uuid
+import datetime as dt
+
 
 # You need a running mongodb instance running 
 # mongo express is useful for visualizing data
@@ -24,104 +26,213 @@ print(os.getenv("MONGO_CONNECTION_STRING"))
 
 
 class DummySchema(Schema):
-    _id = fields.Str(required=True)
-    test_field = fields.Str(required=True)
+    _id = fields.Str(required=False)
+    name = fields.Str(required=True)
+    files = fields.List(fields.Str, required=False)
+    age = fields.Integer(required=True, error_messages={"required": "Age is required."})
+    birthdate = fields.DateTime(default=dt.datetime(2017, 9, 29))
 
 
-def test_insert_one_envset_with_chain_assignment():
+# Id to be used by test funcs
+id1 = str(uuid.uuid4())
 
-    print("Current: ", dm.db_name)
+dummy_data = \
+{
+    "_id": id1,
+    "name": "John",
+    "files": ["f1", "f2"],
+    "age": "20",
+    "birthdate": dt.datetime(2021, 9, 29).strftime( '%Y-%m-%d %H:%M:%S' )
+}
 
-    response, status_code = (
-        dm.db("chaindb") #switch_db_default=False
-        .schema(DummySchema)
-        .collection("chain_collection")
-        .insert_one({ 
-            "_id": str(uuid.uuid4()), 
-            "test_field": "dm db=chaindb collection=chain_collection" 
-        })
-    )
 
-    #dm.switch_to_default_db()
+def test_md_insert_one():
     
-    print("New: ", dm.db_name)
-    print(response)
-
-    assert_that(status_code).is_in(202)
-    assert_that(response).contains_entry({'status': 'success'})
-
-
-
-def test_insert_one_envset_with_chain_assignment_defaultdb():
-
-    print("Should be `db`: ", dm.db_name)
-
-    response, status_code = (
-        dm.schema(DummySchema)
-        .collection("chain_collection")
-        .insert_one({ 
-            "_id": str(uuid.uuid4()), 
-            "test_field": "dm db=default(db) collection=chain_collection" 
-        })
-
+    id_list = MongoData.insert(
+        schema=DummySchema, 
+        collection="testcollection", 
+        data={
+            "_id": id1,
+            "name": "John Show",
+            "files": ["f1", "f2"],
+            "age": "20",
+            "birthdate": dt.datetime(2021, 9, 29).strftime( '%Y-%m-%d %H:%M:%S' )
+        }
     )
     
-    print(response)
-
-    assert_that(status_code).is_in(202)
-    assert_that(response).contains_entry({'status': 'success'})
+    assert_that(id_list).is_not_none().contains_only(id1) 
 
 
 
-def test_insert_one_envset_with_chain_assignment_swichdb():
+def test_md_insert_many():
+    
+    id_list = MongoData.insert(
+        schema=DummySchema, 
+        collection="testcollection", 
+        data=[
+            dict(dummy_data, **{"_id": str(uuid.uuid4())}), 
+            dict(dummy_data, **{"_id": str(uuid.uuid4())}),
+            dict(dummy_data, **{"_id": str(uuid.uuid4())}),
+        ]
+    )
+    
+    print(id_list)
 
-    print("Should be `db`: ", dm.db_name)
+    assert_that(id_list).is_not_none().is_length(3)
 
-    response, status_code = (
-        dm.schema(DummySchema)
-        .collection("chain_collection")
-        .insert_one({ 
-            "_id": str(uuid.uuid4()), 
-            "test_field": "dm db=db collection=chain_collection" 
-        })
 
+
+
+
+def test_md_fetch_one_with_id():
+
+    data_dict = MongoData.fetch(
+        match = id1,
+        collection="testcollection"
+    )
+    
+    print(data_dict)
+
+    assert_that(data_dict).contains_entry({"_id": id1})
+
+
+
+
+def test_md_fetch_all_with_match():
+
+    response = MongoData.fetch(
+        match = {'name': 'John Show'},
+        collection="testcollection"
+    )
+    
+    print(list(response))
+
+
+
+def test_md_update_one_with_id():
+
+    response = MongoData.update(
+        collection = "testcollection",
+        match      = id1,
+        new_data   = {'name': 'New John Show'}
     )
     
     print(response)
 
-    assert_that(status_code).is_in(202)
-    assert_that(response).contains_entry({'status': 'success'})
 
+def test_md_update_all_with_match():
 
-
-def test_insert_one_envset_with_schema_collection():
-
-    dm = DataManagement(schema=DummySchema, collection_name="mycollection")
-
-    data = { 
-        "_id": str(uuid.uuid4()), 
-        "test_field": "added in mycollection with local instance of dm" 
-    }
-
-    response, status_code = dm.insert_one(data)
+    response = MongoData.update(
+        collection ="testcollection",
+        match      = {'name': 'John Show'},
+        new_data   = {'name': 'GOT John Show'}
+    )
 
     print(response)
 
-    assert_that(status_code).is_in(202)
-    assert_that(response).contains_entry({'status': 'success'})
+
+
+
+
+
+
+
+
+
+# def test_insert_one_envset_with_chain_assignment():
+
+#     print("Current: ", dm.db_name)
+
+#     response, status_code = (
+#         dm.db("chaindb") #switch_db_default=False
+#         .schema(DummySchema)
+#         .collection("chain_collection")
+#         .insert_one({ 
+#             "_id": str(uuid.uuid4()), 
+#             "name": "dm db=chaindb collection=chain_collection" 
+#         })
+#     )
+
+#     #dm.switch_to_default_db()
+    
+#     print("New: ", dm.db_name)
+#     print(response)
+
+#     assert_that(status_code).is_in(202)
+#     assert_that(response).contains_entry({'status': 'success'})
+
+
+
+# def test_insert_one_envset_with_chain_assignment_defaultdb():
+
+#     print("Should be `db`: ", dm.db_name)
+
+#     response, status_code = (
+#         dm.schema(DummySchema)
+#         .collection("chain_collection")
+#         .insert_one({ 
+#             "_id": str(uuid.uuid4()), 
+#             "name": "dm db=default(db) collection=chain_collection" 
+#         })
+
+#     )
+    
+#     print(response)
+
+#     assert_that(status_code).is_in(202)
+#     assert_that(response).contains_entry({'status': 'success'})
+
+
+
+# def test_insert_one_envset_with_chain_assignment_swichdb():
+
+#     print("Should be `db`: ", dm.db_name)
+
+#     response, status_code = (
+#         dm.schema(DummySchema)
+#         .collection("chain_collection")
+#         .insert_one({ 
+#             "_id": str(uuid.uuid4()), 
+#             "name": "dm db=db collection=chain_collection" 
+#         })
+
+#     )
+    
+#     print(response)
+
+#     assert_that(status_code).is_in(202)
+#     assert_that(response).contains_entry({'status': 'success'})
+
+
+
+# def test_insert_one_envset_with_schema_collection():
+
+#     dm = DataManagement(schema=DummySchema, collection_name="mycollection")
+
+#     data = { 
+#         "_id": str(uuid.uuid4()), 
+#         "name": "added in mycollection with local instance of dm" 
+#     }
+
+#     response, status_code = dm.insert_one(data)
+
+#     print(response)
+
+#     assert_that(status_code).is_in(202)
+#     assert_that(response).contains_entry({'status': 'success'})
     
 
 
-def test_insert_one_envset_with_schema_collection_db():
+# def test_insert_one_envset_with_schema_collection_db():
 
-    dm = DataManagement(schema=DummySchema, collection_name="mycollection", db_name="newdb")
+#     dm = DataManagement(schema=DummySchema, collection_name="mycollection", db_name="newdb")
 
-    response, status_code = dm.insert_one({ 
-                                    "_id": str(uuid.uuid4()), 
-                                    "test_field": "added in mycollection with local instance of db newdb" 
-                                })
+#     response, status_code = dm.insert_one({ 
+#                                     "_id": str(uuid.uuid4()), 
+#                                     "name": "added in mycollection with local instance of db newdb" 
+#                                 })
 
-    print(response)
+#     print(response)
 
-    assert_that(status_code).is_in(202)
-    assert_that(response).contains_entry({'status': 'success'})
+#     assert_that(status_code).is_in(202)
+#     assert_that(response).contains_entry({'status': 'success'})
